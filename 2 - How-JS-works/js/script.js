6d26cd9dcd9474d143303b7dca49f398da0bf102
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-05-06 18:13:42 +0200

    sección 2 casi completada

--- 2 - How-JS-works/js/script.js
+++ 2 - How-JS-works/js/script.js
@@ -1,20 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+calculateAge(1997);
+
+function calculateAge(year) {
+    console.log(2019 - year);
+}
 
 
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
+
+    retirement(1990);
 
 
+// variables
+console.log(age);
+var age = 23;
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
-
-
-
-
-
-
-
-
-
+*/
 
 
 ///////////////////////////////////////
