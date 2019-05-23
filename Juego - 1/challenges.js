c111ddd9f1c7547c70e5880288ad573abb438319
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-05-24 00:54:43 +0200

    Player can input winning score.

--- Juego - 1/challenges.js
+++ Juego - 1/challenges.js
@@ -54,8 +54,17 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		// Update the UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+		var input = document.querySelector('.final-score').value;
+		var winningScore;
+
+		if (input) {
+			var winningScore = input;
+		} else {
+			winningScore = 100;
+		}
+
 		// Check if Player won the game
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= winningScore) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -66,6 +75,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		}
 	}
 });
+
 
 function nextPlayer() {
 	//Next Player
