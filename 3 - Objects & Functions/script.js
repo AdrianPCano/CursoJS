5dfc04172956a4c21f4b6fae5366c32079638d91
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-05-27 06:26:21 +0200

    Inheritance

---
+++ 3 - Objects & Functions/script.js
@@ -0,0 +1,36 @@
+var john = {
+	name: 'john',
+	yearOfBirth: 1990,
+	job: 'teacher'
+};
+
+var Person = function (name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
+
+Person.prototype.calculateAge = this.calculateAge = function() {
+		console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1988, 'scientist');
+var mark = new Person('Mark', 1986, 'web developer');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
+
+
+
+
+
+
