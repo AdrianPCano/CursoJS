b5aa113783cec9382911c0cecf687da0167de06e
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-05-30 16:59:02 +0200

    Applying  closures to our previous code.

--- 3 - Objects & Functions/script.js
+++ 3 - Objects & Functions/script.js
@@ -177,21 +177,56 @@ game();
 	var score = Math.random() * 10;
 	console.log(score >= 5);
 }) ();
-*/
+
 (function (goodLuck)  {
 	var score = Math.random() * 10;
 	console.log(score >= 5 - goodLuck);
 }) (5);
+*/
+/*
+
+//	Closures
+
+function retirement(retirementAge) {
+	var a = ' years left until retirement.';
+	return function(yearOfBirth) {
+		var age = 2019 - yearOfBirth;
+		console.log((retirementAge - age) + a);
+	}
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
 
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+*/
 
+function intervewQuestion(job) {
+	return function(name) {
+		if (job  === 'designer') {
+				console.log(name + ', can you please explain what UX design is?');
+			
+		} else if (job === 'teacher') {
+				console.log('What subject do you teach, ' + name + '?');
+			
+		} else {
+				console.log('Hello ' + name + ', what do you do?');
+		}
+	}
+}
 
+var teacherQuestion = intervewQuestion('teacher');
+var designerQuestion = intervewQuestion('designer');
+var restQuestion = intervewQuestion('writer');
 
-
-
-
-
-
+teacherQuestion('John');
+designerQuestion('Sara');
+intervewQuestion('teacher')('Adrian');
+intervewQuestion('Futballer')('Messi');
 
 
 
