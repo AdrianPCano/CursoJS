16944e4fa3381b41d6388782103aa52b0323341f
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-06-11 02:13:36 +0200

    Income and Expense Function

--- 4 - Budgety/starter/app.js
+++ 4 - Budgety/starter/app.js
@@ -2,7 +2,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    // Some code
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;   
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;   
+    };
+
+    var data = {
+        allItems: {
+            allExpenses: [],
+            allIncomes: [],
+        },
+        totals: {
+            allExpenses: 0,
+            allIncomes: 0
+        }
+    };
 
 })();
 
@@ -39,7 +60,19 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    };
+
 
     var ctrlAddItem = function() {
 
@@ -55,14 +88,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
+    };
+
+    return {
+        init: function() {
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
-
 })(budgetController, UIController);
+
+controller.init();
